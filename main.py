2cf3c6b7ccba16f7477c38236e8c116de664d9c7
omare <omar.elkobia@gmail.com>
2022-07-26 21:56:07 +0200

    first version 1.0

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+nouns = ['Chair', 'Laptop', 'Book', 'City', 'New York', 'Banana',  'Family', 'Ice cream', 'Table', 'Anger']
+adjectives = ['Blue', 'Red', 'Purple', 'Green', 'Black', 'Cyan', 'Rainbow', 'Marin', 'Mink', 'Orange', ]
+punctuations = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ]
+
+print('-*-*-*-*-*-*-*-*-*-> Welcome to password picker,this app creates a hard password to crack. <-*-*-*-*-*-*-*-*-*-')
+print('')
+
+is_new_password_required = True
+
+while is_new_password_required:
+    noun = random.choice(nouns)
+    adjective = random.choice(adjectives)
+    punctuation = random.choice(punctuations)
+    number = random.randint(0, 100)
+
+    password = f"{noun}{adjective}{number}{punctuation}"
+
+    print(f'Your new password is {password}')
+    answer = input('Do you need another password? Yes/No: ')
+
+    if answer.lstrip().rstrip().lower() == 'yes':
+        is_new_password_required = True
+    elif answer.lstrip().rstrip().lower() == 'no':
+        print('Thanks for using my app.')
+        is_new_password_required = False
+
+    else:
+        is_new_password_required = False
+        print("you've entered a wrong choice")
