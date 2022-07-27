9d69f1b60a55869bb9d5fe5c1cc2835ab8bc7c1e
omare <omar.elkobia@gmail.com>
2022-07-27 12:18:44 +0200

    updated project to run 3 times in the beginning to give you 3 passwords

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 
-nouns = ['Chair', 'Laptop', 'Book', 'City', 'New York', 'Banana',  'Family', 'Ice cream', 'Table', 'Anger']
-adjectives = ['Blue', 'Red', 'Purple', 'Green', 'Black', 'Cyan', 'Rainbow', 'Marin', 'Mink', 'Orange', ]
-punctuations = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ]
+nouns = ['Chair', 'Laptop', 'Book', 'City', 'New York', 'Banana',  'Family', 'Ice cream', 'Table', 'Anger' 'mouse',
+         'cat']
+adjectives = ['Blue', 'Red', 'Purple', 'Green', 'Black', 'Cyan', 'Rainbow', 'Marin', 'Mink', 'Orange', 'perfect',
+              'sharp']
+punctuations = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '~', '`']
 
 print('---------------------------------------------------------------------------------------------------------------')
 print('-*-*-*-*-*-*-*-*-*-> Welcome to password picker,this app creates a hard password to crack. <-*-*-*-*-*-*-*-*-*-')
@@ -12,14 +14,15 @@ print('')
 is_new_password_required = True
 
 while is_new_password_required:
-    noun = random.choice(nouns)
-    adjective = random.choice(adjectives)
-    punctuation = random.choice(punctuations)
-    number = random.randint(0, 100)
+    for x in range(0, 3):
+        noun = random.choice(nouns)
+        adjective = random.choice(adjectives)
+        punctuation = random.choice(punctuations)
+        number = random.randint(0, 100)
 
-    password = f"{noun}{adjective}{number}{punctuation}"
+        password = f"{noun}{adjective}{number}{punctuation}"
+        print(f'Your new password is {password}')
 
-    print(f'Your new password is {password}')
     answer = input('Do you need another password? Yes/No: ')
 
     if answer.lstrip().rstrip().lower() == 'yes':
